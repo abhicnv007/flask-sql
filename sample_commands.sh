a40472fa8cea8a1b5892a17cffeeea4ce7b2f109
Abhishek Chander NV <abhicnv007@gmail.com>
2019-10-18 12:53:17 +0530

    sample commands

---
+++ sample_commands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Best-effort install script prerequisites, otherwise they will need to be installed manually.
+if [[ -f /usr/bin/apt-get && ! -f /usr/bin/jq ]]
+then
+  sudo apt-get update
+  sudo apt-get install -y jq
+fi
+
+
+HOST="localhost:5000"
+
+echo "Trying the authenticate api for access token"
+TOKEN=$(curl http://${HOST}/authenticate -XPOST \
+ -H "Content-type: application/json" \
+ -d '{"username":"default", "password":"iaMAStronGP@$$w0rd"}' | jq -r '.access_token')
+
+echo "access token" $TOKEN
+
+echo 'Trying bank details for IFSC HDFC0000001'
+curl -H "Authorization: jwt ${TOKEN}" http://${HOST}/bank_details?ifsc_code=HDFC0000001
+
+echo 'Trying branch details for State bank of india in Mumbai' 
+curl -H "Authorization: jwt ${TOKEN}" \
+    http://${HOST}/branch_details?bank_name=State%20Bank%20of%20India\&city=MUMBAI\&limit=10\&offset=100
